a26a4d1ab8768f640c37764bf5571805283c7eb5
Michael Ellenburg <michael.ellenburg@mesosphere.io>
2016-04-26 02:12:54 -0700

    Update setup with docker install

--- system/setup.sh
+++ system/setup.sh
@@ -7,3 +7,16 @@ git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 #syntastic uses local flake8 binary
 #syntastic will not accept aliases. Needs to have flake8 on path
 sudo cp flake8 /usr/bin/local/flake8
+#docker installation
+sudo apt-get install apt-transport-https ca-certificates
+sudo apt-key adv --keyserver hkp://p80.pool.sks-keyservers.net:80 --recv-keys 58118E89F3A912897C070ADBF76221572C52609D
+# this command needs some love
+#sudo cat 'deb https://apt.dockerproject.org/repo ubuntu-wily main' > /etc/apt/sources.list.d/docker.list
+sudo apt-get update
+sudo apt-get purge lxc-docker
+apt-cache policy docker-engine
+sudo apt-get install linux-image-extra-$(uname -r)
+sudo apt-get install docker-engine
+sudo service docker start
+sudo groupadd docker
+sudo usermod -aG docker michael
