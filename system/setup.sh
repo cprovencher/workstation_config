27c605c0a1d1901bad74db9abf6ea603bb291f8a
Michael Ellenburg <michael.ellenburg@mesosphere.io>
2016-04-26 02:00:11 -0700

    Add credential caching

--- system/setup.sh
+++ system/setup.sh
@@ -2,6 +2,7 @@
 sudo apt-get install -y vim tmux flake8
 git config --global user.email "michael.ellenburg@mesosphere.io"
 git config --global user.name "Michael Ellenburg"
+git config --global credential.helper cache
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 #syntastic uses local flake8 binary
 #syntastic will not accept aliases. Needs to have flake8 on path
