e286b656a9717014d2737123b8d878abb0f82b78
Michael Ellenburg <michael.ellenburg@mesosphere.io>
2016-10-28 11:48:43 -0700

    Cleanup

--- system/setup.sh
+++ system/setup.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
-sudo apt-get install -y vim tmux flake8
+sudo apt-get install -y vim git tmux python-flake8
 git config --global user.email "michael.ellenburg@mesosphere.io"
 git config --global user.name "Michael Ellenburg"
 git config --global credential.helper cache
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 #syntastic uses local flake8 binary
 #syntastic will not accept aliases. Needs to have flake8 on path
+sudo mkdir -p /usr/bin/local
 sudo cp flake8 /usr/bin/local/flake8
 #docker installation
 sudo apt-get install apt-transport-https ca-certificates
